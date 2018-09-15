5f2ca7cc2e5ce491a16b93d5888a4e9cb5bc33ac
Daniel Toebe <dtoebe@gmail.com>
2018-09-15 05:34:24 -0700

    Finish data

--- dtoebe/rust/restapi/src/data/mod.rs
+++ dtoebe/rust/restapi/src/data/mod.rs
@@ -1,10 +1,11 @@
 extern crate serde_derive;
 extern crate serde_json as json;
 
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Comment {
     pub id: u32,
     pub name: String,
@@ -12,20 +13,73 @@ pub struct Comment {
     pub body: String,
 }
 
+impl Comment {
+    pub fn new(name: String, email: String, body: String) -> Comment {
+        Comment {
+            id: 123,
+            name: name,
+            email: email,
+            body: body,
+        }
+    }
+
+    fn new_with_id(id: u32, name: String, email: String, body: String) -> Comment {
+        Comment {
+            id: id,
+            name: name,
+            email: email,
+            body: body,
+        }
+    }
+}
+
 pub struct Data {
-    pub comments: Vec<Comment>,
+    pub comments: HashMap<u32, Comment>,
     pub file_path: String,
 }
 
 impl Data {
     pub fn new(path: String) -> Result<Data, Box<Error>> {
         let file = File::open(&path)?;
-        let comments = json::from_reader(file)?;
+        let com: Vec<Comment> = json::from_reader(file)?;
+        let mut comments = HashMap::new();
+
+        for c in com {
+            comments.insert(c.id, c);
+        }
 
         Ok(Data {
             comments: comments,
             file_path: path,
         })
     }
-}
 
+    pub fn add_comment(&mut self, comment: Comment) -> Comment {
+        self.comments.insert(comment.id, comment.clone());
+        comment
+    }
+
+    pub fn update_comments(
+        &mut self,
+        id: u32,
+        name: String,
+        email: String,
+        body: String,
+    ) -> Result<Comment, &'static str> {
+        match self.comments.get(&id.clone()) {
+            Some(_) => {
+                self.comments.insert(
+                    id.clone(),
+                    Comment::new_with_id(id.clone(), name, email, body),
+                );
+                Ok(self.comments[&id].clone())
+            }
+            None => Err("id not found"),
+        }
+    }
+
+    pub fn delete_comments(&mut self, comment: Comment) -> Result<Comment, Box<Error>> {
+        self.comments.remove(&comment.id);
+        Ok(comment)
+    }
+}
